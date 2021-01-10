952e334640a9a1a663e15800fc0245aa78cc4c89
shikhar <shikhar21121999@gmail.com>
2021-01-10 23:17:08 +0530

    made default app ui work yet to be done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+// import { Button,Card } from 'react-bootstrap';
+import MyForm from './MyForm';
 
-function App() {
+export default function App() {
+  const [searchlocation,Setsearchlocation] = useState("london")
+  const [temp,setTemp] = useState(55)
+  const [region,setregion] = useState("my area")
+
+  useEffect(() => {
+    //we call the weather app api to get the data for current location
+    
+    // destructure to get the url to search for location
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    var url="https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=d72c618a6ee745cb83e164815211001&q="
+    url+=searchlocation;
+
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data.current.temp_c);
+        setTemp(data.current.temp_c)
+        setregion(data.location.region)
+      })
+      .catch(error => console.log('error', error));
+  
+  },[searchlocation]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyForm Setsearchlocation={Setsearchlocation} temp={temp} region={region}/>
+      <div></div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/MyForm.js
@@ -0,0 +1,29 @@
+import React, {useState,useRef} from 'react'
+import {Form} from 'react-bootstrap'
+export default function MyForm({Setsearchlocation,temp,region}) {
+    const input_search = useRef();
+    
+    // renders a form with a input search
+    // and a search button
+
+    const handlesubmit = (e) =>{
+        // arrow function to handel form submission
+        e.preventDefault();
+        console.log("form submitted")
+        console.log(input_search.current.value)
+        Setsearchlocation(input_search.current.value)
+    } 
+
+
+    return (
+        <div className="d-flex" style={{ height: '100vh' }}>
+            <Form onSubmit={handlesubmit}>
+                <Form.Group>
+                <Form.Control ref={input_search} type="input" placeholder="Enter the location" />
+                </Form.Group>
+            </Form>
+            <div>{temp}</div>
+            <div>{region}</div>
+        </div>
+    )
+}
